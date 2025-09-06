c791fb777944849988417dde571bc033517e6474
Cristofer-SanMartin-Dev <cri.sanmartin96@gmail.com>
2025-09-06 14:10:18 -0400

    imagenes de los productos

--- Huerto Hogar/JS/data.js
+++ Huerto Hogar/JS/data.js
@@ -6,7 +6,7 @@ const products = [
         description: 'Manzanas Fuji crujientes y dulces, cultivadas en el Valle del Maule. Perfectas para meriendas.',
         stock: 150,
         category: 'Frutas Frescas',
-        imageUrl: 'https://images.unsplash.com/photo-1568702846914-96b305d2aaeb?q=80&w=1770'
+        imageUrl: '../IMG/manzana.png'
     },
     {
         id: 'FR002',
@@ -15,7 +15,7 @@ const products = [
         description: 'Jugosas y ricas en vitamina C, estas naranjas Valencia son ideales para zumos frescos.',
         stock: 200,
         category: 'Frutas Frescas',
-        imageUrl: 'https://images.unsplash.com/photo-1611080626919-7cf5a9dbab5b?q=80&w=1935'
+        imageUrl: '../IMG/naranja.png'
     },
     {
         id: 'VR001',
@@ -24,7 +24,7 @@ const products = [
         description: 'Zanahorias crujientes cultivadas sin pesticidas en la Región de O\'Higgins.',
         stock: 100,
         category: 'Verduras Orgánicas',
-        imageUrl: 'https://images.unsplash.com/photo-1590431306482-f700ee050c59?q=80&w=1770'
+        imageUrl: '../IMG/zanahoria.png'
     },
     {
         id: 'PO001',
@@ -33,7 +33,7 @@ const products = [
         description: 'Miel pura y orgánica producida por apicultores locales. Rica en antioxidantes.',
         stock: 50,
         category: 'Productos Orgánicos',
-        imageUrl: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?q=80&w=1975'
+        imageUrl: '../IMG/miel.png'
     },
     {
         id: 'PL001',
@@ -42,7 +42,7 @@ const products = [
         description: 'Leche fresca y cremosa de granjas locales, ideal para un desayuno nutritivo.',
         stock: 75,
         category: 'Productos Lácteos',
-        imageUrl: 'https://images.unsplash.com/photo-1563637123995-f93c8d350612?q=80&w=1887'
+        imageUrl: '../IMG/leche.png'
     },
     {
         id: 'VR003',
@@ -51,6 +51,6 @@ const products = [
         description: 'Pimientos rojos, amarillos y verdes, ideales para salteados y platos coloridos. Ricos en antioxidantes.',
         stock: 120,
         category: 'Verduras Orgánicas',
-        imageUrl: 'https://images.unsplash.com/photo-15187363408-2d7494491970?q=80&w=1770'
+        imageUrl: '../IMG/pimenton.png'
     }
 ];
