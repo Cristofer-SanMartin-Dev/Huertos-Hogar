a533033f432ab94eb5768ebfe84b27c5dbec2a6d
Cristofer-SanMartin-Dev <cri.sanmartin96@gmail.com>
2025-09-28 12:52:33 -0300

    Integración: React Router para navegación entre páginas principales

--- Huerto Hogar Experiencia 2/src/App.jsx
+++ Huerto Hogar Experiencia 2/src/App.jsx
@@ -1,23 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
-import { HomePage } from './pages/HomePage';
-import { ProductsPage } from './pages/ProductsPage';
-import { CartPage } from './pages/CartPage';
-import { OrderSuccessPage } from './pages/OrderSuccessPage';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import { CartProvider } from './context/CartContext';
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:categoryId" element={<ProductsPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/order-success" element={<OrderSuccessPage />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/productos" element={<ProductsPage />} />
+          <Route path="/carrito" element={<CartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/perfil" element={<ProfilePage />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
-}
+};
 
 export default App;
