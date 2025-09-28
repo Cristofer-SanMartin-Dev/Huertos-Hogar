7305f5d147e59e25559680bdcbc28304ea7cbd0d
Cristofer-SanMartin-Dev <cri.sanmartin96@gmail.com>
2025-09-28 20:59:05 -0300

    Configuración Inicial, Estructura y Layout Principal

--- huerto-hogar-react/src/main.jsx
+++ huerto-hogar-react/src/main.jsx
@@ -1,21 +1,21 @@
-<<<<<<< HEAD
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+
+// Importamos los Contextos (Estado global)
 import { CartProvider } from './context/CartContext';
-// 1. Importa el AuthProvider
 import { AuthProvider } from './context/AuthContext';
 
+// Importamos los estilos
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* 2. Envuelve la app con AuthProvider */}
-      {/* Es buena idea que AuthProvider envuelva a CartProvider si el carrito depende del usuario */}
+      {/* AuthProvider envuelve a CartProvider, y este a la App */}
       <AuthProvider>
         <CartProvider>
           <App />
@@ -24,15 +24,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> af2fc13 (Extructura base)
