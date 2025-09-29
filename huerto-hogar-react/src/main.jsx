dc4f22551813f1e5a641814964e88732bcdc82ed
Cristofer-SanMartin-Dev <cri.sanmartin96@gmail.com>
2025-09-28 21:41:09 -0300

    Formularios de Autenticación (LoginPage y RegisterPage)

--- huerto-hogar-react/src/main.jsx
+++ huerto-hogar-react/src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
+// 1. Importa el AuthProvider
+import { AuthProvider } from './context/AuthContext';
 
 // Importamos los Contextos (Estado global)
 import { CartProvider } from './context/CartContext';
