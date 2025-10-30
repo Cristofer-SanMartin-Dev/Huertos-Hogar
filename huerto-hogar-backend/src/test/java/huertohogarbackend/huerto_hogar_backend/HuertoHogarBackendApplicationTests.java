fb785555e8f2030c51d99c2dcdd89f77a7151163
Cristofer-SanMartin-Dev <cri.sanmartin96@gmail.com>
2025-10-30 20:13:20 -0300

    Configuración Inicial del Proyecto

---
+++ huerto-hogar-backend/src/test/java/huertohogarbackend/huerto_hogar_backend/HuertoHogarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package huertohogarbackend.huerto_hogar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuertoHogarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
